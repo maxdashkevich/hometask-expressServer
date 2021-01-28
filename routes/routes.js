e11beb3cfea05e3ea2e51223d7e4ff20c68097aa
Maksim Dashkevich <maximkacr7@gmail.com>
2021-01-28 16:59:52 +0300

    add multer as middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import multer from 'multer';
 export const router = express.Router();
 
 import {UsersController} from '../controllers/controller.js';
 import {auth} from '../middlewares/auth.middleware.js';
 import {validate} from '../middlewares/validation.middleware.js';
 import { createUserSchema, updateUserSchema } from '../validation-schemes/user.scheme.js';
+import { upload } from "../middlewares/multer.middleware.js";
 
-const upload = multer({dest: 'public/avatar/' })
 const controller = new UsersController();
 
 router
@@ -16,5 +15,5 @@ router
     .put('/:id', auth('admin'), validate(updateUserSchema), controller.update)
     .delete('/:id', controller.delete)
     .post('/auth', controller.login)
-    .post('/:id/avatar', upload.single('avatar'), controller.addAvatar)
+    .post('/:id/avatar', upload, controller.addAvatar)
     .get('/:id/avatar', controller.getAvatar);
